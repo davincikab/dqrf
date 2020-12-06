ebf869dfd8881c6dbad5e624b2fb687c5401db3f
davincikab <davidnganganjeri079@gmail.com>
2020-12-06 08:39:00 +0300

    alert list view

--- alerts/views.py
+++ alerts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+
+from .models import Alert
 
 # Create your views here.
 @login_required
 def main_view(request):
     return render(request, 'alerts/index.html')
+
+@login_required
+def alert_list(request):
+    alerts = Alert.objects.all()
+    return render(request, 'alerts/list.html', {'alerts':alerts})
